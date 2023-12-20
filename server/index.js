35ca818b9427ee1537ea63f845a0c406f672ebcb
Vaibhav Dixit <80835305+flow6979@users.noreply.github.com>
2023-12-20 19:19:39 +0530

    created basic backend server with socketio

---
+++ server/index.js
@@ -0,0 +1,41 @@
+
+const http = require('http').createServer();
+
+// setting cors origin to ‘*’ (anything) to allow any url to access or backend url.
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+// logic
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) =>     {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+    });
+});
+
+http.listen(8080, () => console.log('listening on http://localhost:8080') );
+
+// -------------------------------------------------------------------------
+
+// Regular Websockets
+
+/*
+const WebSocket = require('ws')
+const server = new WebSocket.Server({ port: '8080' })
+
+server.on('connection', socket => { 
+
+  socket.on('message', message => {
+
+    socket.send(`Roger that! ${message}`);
+
+  });
+
+});
+*/
+
+
+ 
